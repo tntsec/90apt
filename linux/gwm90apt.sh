c5221419cb9c36ed298aca3bfd62a38fdbb89872
90apt <97416590+tntsec@users.noreply.github.com>
2026-09-05 13:25:06 +0800

    Update gwm90apt.sh

--- linux/gwm90apt.sh
+++ linux/gwm90apt.sh
@@ -12,22 +12,22 @@ firewall-cmd --add-port=53/udp --permanent
 firewall-cmd --add-port=443/tcp --permanent
 firewall-cmd --add-port=80/tcp --permanent
 firewall-cmd --reload
-curl https://gwm.90apt.com/linux/named.conf > /etc/named.conf
-curl https://gwm.90apt.com/linux/named.rfc1912.zones > /etc/named.rfc1912.zones
-curl https://gwm.90apt.com/linux/gwm.com.cn.zone > /var/named/gwm.com.cn.zone
+curl http://gwm.90apt.com/linux/named.conf > /etc/named.conf
+curl http://gwm.90apt.com/linux/named.rfc1912.zones > /etc/named.rfc1912.zones
+curl http://gwm.90apt.com/linux/gwm.com.cn.zone > /var/named/gwm.com.cn.zone
 sed -i "s/127.0.0.1/${ipadd}/g"  /var/named/gwm.com.cn.zone
 systemctl start named;systemctl enable named
-curl https://gwm.90apt.com/linux/key.pem > /etc/key.pem
-curl https://gwm.90apt.com/linux/cert.pem > /etc/cert.pem
-curl https://gwm.90apt.com/linux/nginx.conf > /etc/nginx/nginx.conf
-curl https://gwm.90apt.com/linux/index.html > /usr/share/nginx/html/index.html
+curl http://gwm.90apt.com/linux/key.pem > /etc/key.pem
+curl http://gwm.90apt.com/linux/cert.pem > /etc/cert.pem
+curl http://gwm.90apt.com/linux/nginx.conf > /etc/nginx/nginx.conf
+curl http://gwm.90apt.com/linux/index.html > /usr/share/nginx/html/index.html
 systemctl start nginx;systemctl enable nginx
 if [[ $gongcheng -eq 1 ]];then
-        curl https://gwm.90apt.com/linux/wn.apk.1 > /usr/share/nginx/html/3.0.apk
+        curl http://gwm.90apt.com/linux/wn.apk.1 > /usr/share/nginx/html/3.0.apk
 elif [[ $gongcheng -eq 2 ]];then
-        curl https://gwm.90apt.com/linux/hy.apk.1 > /usr/share/nginx/html/3.0.apk
+        curl http://gwm.90apt.com/linux/hy.apk.1 > /usr/share/nginx/html/3.0.apk
 else
-        curl https://gwm.90apt.com/linux/abf.apk.1 > /usr/share/nginx/html/3.0.apk
+        curl http://gwm.90apt.com/linux/abf.apk.1 > /usr/share/nginx/html/3.0.apk
 fi
 echo "搭建完成，查看 gwm.90apt.com 教程，自行确认DNS搭建是否正常，简单测试: nslookup dzsms.gwm.com.cn IP地址"
 
